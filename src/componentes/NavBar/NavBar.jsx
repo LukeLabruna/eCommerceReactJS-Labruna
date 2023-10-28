381f6a0a90494cc378781f8e5e97948ca7df3fe8
Lucas Labruna <lucaspablolabruna@gmail.com>
2023-10-28 01:27:44 -0300

    se agregaron estilos y eventos para hacer la aplicacion responsive

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { useState, useEffect, useRef } from "react"
 import CartWidget from "../CartWidget/CartWidget"
 import BarraBusqueda from '../BarraBusqueda/BarraBusqueda'
 import { Link } from 'react-router-dom'
 import "./NavBar.css"
 
 const NavBar = () => {
+  const [mostrarMenu, setMostrarMenu] = useState(false)
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickAfuera = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMostrarMenu(false);
+      }
+    }
+
+    const handleScroll = () => {
+      setMostrarMenu(false); // Cierra el menú al hacer scroll
+    }
+
+    document.addEventListener("click", handleClickAfuera);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("click", handleClickAfuera);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleOnClickMenu = (e) => {
+    e.stopPropagation();
+    setMostrarMenu(!mostrarMenu)
+  }
+
   return (
     <header className='navBar'>
       <Link to="/">
@@ -13,16 +41,19 @@ const NavBar = () => {
           <h1>J.R.R. Tolkien</h1>
         </div>
       </Link>
-      <nav>
-        <ul className='categorias'>
-          <Link to="/categoria/libros" ><li>Libros</li></Link>
-          <Link to="/categoria/peliculas" ><li>Peliculas</li></Link>
-          <Link to="/categoria/musica" ><li>Musica</li></Link>
-          <Link to="/categoria/merch" ><li>Merch</li></Link>
-        </ul>
-        <BarraBusqueda />
-      </nav>
-      <Link to="/carrito+de+compras"><CartWidget /></Link>
+      <span className="material-symbols-outlined menu " onClick={handleOnClickMenu}>menu</span>
+      <div className={`gridNav${mostrarMenu ? " mostrar" : ""}`} ref={menuRef}>
+        <nav className={`categoria`}>
+          <ul className='categorias'>
+            <Link to="/categoria/libros" ><li>Libros</li></Link>
+            <Link to="/categoria/peliculas" ><li>Peliculas</li></Link>
+            <Link to="/categoria/musica" ><li>Musica</li></Link>
+            <Link to="/categoria/merch" ><li>Merch</li></Link>
+          </ul>
+          <BarraBusqueda />
+        </nav>
+        <Link to="/carrito+de+compras" className={`carrito`}><CartWidget /></Link>
+      </div>
     </header>
   )
 }
