a1aabfd37191a72bd6ebb2093a4ac3e53c38b30e
Lucas Labruna <lucaspablolabruna@gmail.com>
2023-11-17 16:31:07 -0300

    se actualizo ItemCount

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -4,21 +4,24 @@ import "./ItemCount.css";
 
 const ItemCount = ({ inicial, stock, onAdd }) => {
   const [cantidad, setCantidad] = useState(inicial);
+  const [nuevoStock, setNuevoStock] = useState(stock)
   
   const handleIncrementar = () => {
     if (cantidad < stock) {
       setCantidad(cantidad + 1)
+      setNuevoStock(nuevoStock - 1)
     }
   }
   const handleDecrementar = () => {
     if (cantidad > 1) {
       setCantidad(cantidad - 1)
+      setNuevoStock(nuevoStock + 1)
     }
   }
 
   return (
     <div className='ItemCount'>
-      <p>Se encuentra disponible {stock} unidades de este producto </p>
+      <p>Se encuentra disponible {nuevoStock - inicial} unidades de este producto </p>
       <div className='botonesContador'>
         <button className='botonContador' onClick={handleDecrementar}>-</button>
         <p className='cantidadContador'>{cantidad}</p>
