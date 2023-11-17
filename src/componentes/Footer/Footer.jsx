6334d8c10a956f4898a8aea96ec9808d1cd01746
Lucas Labruna <lucaspablolabruna@gmail.com>
2023-11-17 17:54:37 -0300

    se agrego la accion de ir al top de la aplicacion

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -3,47 +3,58 @@ import BarraBusqueda from "../BarraBusqueda/BarraBusqueda"
 import "./Footer.css"
 
 const Footer = () => {
+
+  const handleClick = () => {
+    window.scrollTo(0, 0)
+  }
+
   return (
-    <footer>
-      <Link to="/">
-        <div className='footerBrand'>
-          <img src={"../logo.png"} alt="Logo de J.R.R. Tolkien" />
-          <h2>J.R.R. Tolkien</h2>
+    <>
+    <div className="top" onClick={handleClick}>
+    <i class="fa-solid fa-angles-up"></i>
+    <p>Top</p>
+    </div>
+      <footer>
+        <Link to="/">
+          <div className='footerBrand'>
+            <img src={"../logo.png"} alt="Logo de J.R.R. Tolkien" />
+            <h2>J.R.R. Tolkien</h2>
+          </div>
+        </Link>
+        <div className="footerContactanos">
+          <h3>Contactanos</h3>
+          <p>Atencion al cliente de lunes a viernes de 8 a 20 hs</p>
+          <p><span class="material-symbols-outlined">mail</span> ejemplo@ejemplo.com</p>
+          <p><span class="material-symbols-outlined">call</span> 1234567890</p>
         </div>
-      </Link>
-      <div className="footerContactanos">
-        <h3>Contactanos</h3>
-        <p>Atencion al cliente de lunes a viernes de 8 a 20 hs</p>
-        <p><span class="material-symbols-outlined">mail</span> ejemplo@ejemplo.com</p>
-        <p><span class="material-symbols-outlined">call</span> 1234567890</p>
-      </div>
-      <div className="footerProductos">
-        <h3>Productos</h3>
-        <Link to="/productos">Todos los productos</Link>
-        <Link to="/productos/libros" >Libros</Link>
-        <Link to="/productos/peliculas" >Peliculas</Link>
-        <Link to="/productos/musica" >Musica</Link>
-        <Link to="/productos/merch" >Merch</Link>
-      </div>
-      <div className="footerBusqueda">
-        <h3>Encontra lo que estas buscando</h3>
-        <BarraBusqueda />
-      </div>
-      <div className="footterUsuario">
-        <h3>Usuarios</h3>
-        <Link to="/login">Iniciar sesion</Link>
-        <Link to="/crar+usuario">Crear Usuario</Link>
-      </div>
-      <div className="footerRedes">
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-x-twitter"></i>
-        <i class="fa-brands fa-tiktok"></i>
-      </div>
-      <div className="footerCopyright">
-        <p>J.R.R. Tolkien <i class="fa-regular fa-copyright"></i> 2023. Todos los derechos reservados.</p>
-      </div>
-    </footer>
+        <div className="footerProductos">
+          <h3>Productos</h3>
+          <Link to="/productos">Todos los productos</Link>
+          <Link to="/productos/libros" >Libros</Link>
+          <Link to="/productos/peliculas" >Peliculas</Link>
+          <Link to="/productos/musica" >Musica</Link>
+          <Link to="/productos/merch" >Merch</Link>
+        </div>
+        <div className="footerBusqueda">
+          <h3>Encontra lo que estas buscando</h3>
+          <BarraBusqueda />
+        </div>
+        <div className="footterUsuario">
+          <h3>Usuarios</h3>
+          <Link to="/login">Iniciar sesion</Link>
+          <Link to="/crar+usuario">Crear Usuario</Link>
+        </div>
+        <div className="footerRedes">
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-x-twitter"></i>
+          <i class="fa-brands fa-tiktok"></i>
+        </div>
+        <div className="footerCopyright">
+          <p>J.R.R. Tolkien <i class="fa-regular fa-copyright"></i> 2023. Todos los derechos reservados.</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
